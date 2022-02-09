d57d3e38cb4c217871c8e967a5ca2e047c0048e4
Sergei Golds <sergei.golds@gmail.com>
2022-02-09 22:46:30 +0200

    Added database

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
+
+Route::get('/register', [RegisterController::class, 'index']);
 
 Route::get('/posts', function () {
     return view('posts.index');
