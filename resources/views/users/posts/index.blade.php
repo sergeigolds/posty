094db4cff25e702a72d05eb9cd98005f5ad9a313
Sergei Golds <sergei.golds@gmail.com>
2022-02-20 18:28:32 +0200

    Single post page

---
+++ resources/views/users/posts/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layots.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-8/12">
+
+            <div class="p-6">
+                <h1 class="text-2xl font-medium mb-1">{{ $user->name }}</h1>
+            </div>
+
+            <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }} and received {{ $user->receivedLikes->count() }} likes</p>
+
+            <div class="bg-white p-6 rounded">
+                @if ($posts->count())
+                    @foreach ($posts as $post)
+                        <x-post :post="$post" />
+                    @endforeach
+
+                    {{ $posts->links() }}
+
+                @else
+                    <p>{{ $user->name }} Does not have any posts</p>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostLikeController;
+use App\Http\Controllers\UserPostController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
@@ -15,6 +16,8 @@ Route::get('/', function () {
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard')
     ->middleware('auth');
+
+Route::get('/users/{user:username}/posts', [UserPostController::class, 'index'])->name('user.posts');
 
 Route::get('/login', [LoginController::class, 'index'])
     ->name('login')
@@ -31,9 +34,9 @@ Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 Route::get('/posts', [PostController::class, 'index'])
     ->name('posts')
     ->middleware('auth');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts', [PostController::class, 'store']);
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
 
 Route::post('/posts/{post}/likes', [PostLikeController::class, 'store'])->name('posts.likes');
 Route::delete('/posts/{post}/likes', [PostLikeController::class, 'destroy'])->name('posts.likes');
-
